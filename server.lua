5ac530e549452c6e5e83506d49bee057fc90d52f
btwlouis <56355239+btwlouis@users.noreply.github.com>
2020-07-28 21:14:10 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,9 +5,19 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent("rasen:pay")
 AddEventHandler("rasen:pay", function(points)
-	local _source = source
+    local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
+    
+    if points < 0 then
+	-- ban player or log it
+	print('rasen: ' .. xPlayer.identifier .. ' attempted to exploit the rasen script!')
+	return
+    end
+
+    if points > 10000
+	print('rasen: ' .. xPlayer.identifier .. ' probably attempted to exploit the rasen script!')
+	return		
+    end
+		
     xPlayer.addMoney(points * 10)
-
-
 end)
